e6439876914f640947ff2ed76062c82a3a319b25
Arnulf Rupp <arnulf.rupp@web.de>
2023-06-25 16:38:41 +0200

    Invalid log level reference and not needed USB code removed

--- samples/openthread/ThreadBleSecure/src/main.c
+++ samples/openthread/ThreadBleSecure/src/main.c
@@ -44,7 +44,8 @@
 
 #include "x509_cert_key.h"
 
-LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
+#define LOG_LEVEL LOG_LEVEL_INF
+LOG_MODULE_REGISTER(bbtc_cli_sample);
 
 struct openthread_context *myOpenThreadContext;
 otInstance *myOpenThreadInstance = NULL;
@@ -104,41 +105,6 @@ void HandleBleSecureReceive(otMessage *aMessage, void *aContext)
 
 int main(void)
 {
-#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
-	int ret;
-	const struct device *dev;
-	uint32_t dtr = 0U;
-
-	ret = usb_enable(NULL);
-	if (ret != 0) {
-		LOG_ERR("Failed to enable USB");
-		return;
-	}
-
-	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
-	if (dev == NULL) {
-		LOG_ERR("Failed to find specific UART device");
-		return;
-	}
-
-	LOG_INF("Waiting for host to be ready to communicate");
-
-	/* Data Terminal Ready - check if host is ready to communicate */
-	while (!dtr) {
-		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-		if (ret) {
-			LOG_ERR("Failed to get Data Terminal Ready line state: %d", ret);
-			continue;
-		}
-		k_msleep(100);
-	}
-
-	/* Data Carrier Detect Modem - mark connection as established */
-	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
-	/* Data Set Ready - the NCP SoC is ready to communicate */
-	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
-#endif
-
 	myOpenThreadContext = openthread_get_default_context();
 	myOpenThreadInstance = myOpenThreadContext->instance;
 
@@ -157,5 +123,6 @@ int main(void)
 	otBleSecureStart(myOpenThreadInstance, HandleBleSecureClientConnect, HandleBleSecureReceive,
 			 true, NULL);
 	otBleSecureTcatStart(myOpenThreadInstance, "SECRET", NULL, NULL);
-
+	
+	return 1;
 }
